d0686711f56b9ca570cec75d4bcf650ea6b01fd4
Enes <nsyagz@gmail.com>
2023-11-22 23:28:56 +0300

    solved snow raining problem

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const cover = document.querySelector('.backgroundimage');
 
-    for (let i = 0; i < 100; i++) {
+    for (let i = 1; i < 100; i++) {
         const snowFlake = document.createElement('div');
         snowFlake.className = 'snow';
         cover.appendChild(snowFlake);
@@ -14,12 +14,23 @@ document.addEventListener('DOMContentLoaded', function () {
 
         const delay = Math.random();
         snowFlake.style.animation = `snowRain 5s linear ${delay}s infinite`;
+        console.log("delay" + delay);
 
         const left = Math.random() * 100;
         snowFlake.style.left = `${left}vw`;
 
         const animationDuration = 5 + Math.random() * 5;
         snowFlake.style.animationDuration = `${animationDuration}s`;
+
+        // Kar tanelerini ilk 3 saniye boyunca görünmez yap
+        snowFlake.style.opacity = 0;
+        snowFlake.style.animationPlayState = 'running';
+
+        // Zamanlayıcı ile 3 saniye sonra görünürlüğü aç
+        setTimeout(() => {
+            snowFlake.style.opacity = 1;
+            snowFlake.style.animationPlayState = 'running';
+        }, 1000);
     }
 });
 
