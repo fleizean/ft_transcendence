2cb4a31d909a718a1e3a66f48e6767cfc4205a6f
fyurtsev <furkan1yurtseven57@gmail.com>
2024-01-07 07:34:24 +0300

    models-admin

--- chatPractice/myproject/myapp/admin.py
+++ chatPractice/myproject/myapp/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Room, ChatUser, Message
 
 # Register your models here.
+admin.site.register(Room)
+
+@admin.register(ChatUser)
+class ChatUserAdmin(admin.ModelAdmin):
+    list_display = ["user", "room"]
+    
+    class Meta:
+        model = ChatUser
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ["user", "room", "created_date"]
+    
+    class Meta:
+        model = Message
--- chatPractice/myproject/myapp/models.py
+++ chatPractice/myproject/myapp/models.py
@@ -1,5 +1,19 @@
+import uuid
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
 class Room(models.Model):
-    id = models.UUIDField(verbose_name = "Oda Id")
+    id = models.UUIDField(primary_key = True, default = uuid.uuid4)
+
+class ChatUser(models.Model):
+    user = models.ForeignKey(User, related_name = "chat_user", verbose_name = "Kullanıcı", on_delete = models.CASCADE)
+    room = models.ForeignKey(Room, related_name = "chat_users", verbose_name = "Oda", on_delete = models.CASCADE)
+
+class Message(models.Model):
+    user = models.ForeignKey(User, related_name = "messages", verbose_name = "Kullanıcı", on_delete = models.CASCADE)
+    room = models.ForeignKey(Room, related_name = "messages", verbose_name = "Oda", on_delete = models.CASCADE)
+    content = models.TextField(verbose_name = "Mesaj içeriği")
+    created_date = models.DateField(auto_now_add = True)
+
+
