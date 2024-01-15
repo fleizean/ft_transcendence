6232b91a509a782fe3a77dbc3298506a00426ec3
fyurtsev <furkan1yurtseven57@gmail.com>
2024-01-15 06:16:56 +0300

    sda

--- indianpong/pong/consumers.py
+++ indianpong/pong/consumers.py
@@ -698,49 +698,45 @@ class PongConsumer(AsyncWebsocketConsumer):
 '''
 
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = "chat_%s" % self.room_name
         
         # Join room group
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name, self.channel_name
-        )
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
 
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, close_code):
+    async def disconnect(self, close_code):
         # Leave room group
-        async_to_sync(self.channel_layer.group_discard)(
-            self.room_group_name, self.channel_name
-        )
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     # Receive message from WebSocket
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
 
-        user = UserProfile.objects.get(username = text_data_json["user"])
-        m = Message.objects.create(content=message, user=user, room_id=self.room_name)
+        user = await UserProfile.objects.aget(username = text_data_json["user"])
+        m = await Message.objects.acreate(content=message, user=user, room_id=self.room_name) #? Room object isn't created yet
         # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             self.room_group_name, 
             {
                 "type": "chat_message", 
                 "message": message,
                 "user": user.username,
-                "created_date": m.get_short_date(),
+                "created_date": m.get_short_date(), #? blocking?
             }
         )
 
     # Receive message from room group
-    def chat_message(self, event):
+    async def chat_message(self, event):
         message = event["message"]
         user = event["user"]
         created_date = event["created_date"]
         # Send message to WebSocket
-        self.send(text_data=json.dumps({
+        await self.send(text_data=json.dumps({
             "message": message,
             "user": user,
             "created_date": created_date,
