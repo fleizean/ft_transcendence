bfc1dbab0b06b5e8bc0a4ff8bf83f35320b4ed29
Enes <nsyagz@gmail.com>
2024-02-01 23:26:51 +0300

    Co-authored-by: Aktaş <yeaktas@users.noreply.github.com>

--- server.js
+++ server.js
@@ -6,9 +6,8 @@ const app = express();
 app.use("/assets", express.static(path.resolve(__dirname, "frontend", "assets")));
 app.use("/css", express.static(path.resolve(__dirname, "frontend", "css")));
 app.use("/js", express.static(path.resolve(__dirname, "frontend", "js")));
-
 app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "baseindex.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
 });
 
 app.listen(process.env.PORT || 8081, () => console.log("Server running..."));
