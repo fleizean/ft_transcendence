1c1d49c1b9aca24592825b114d4ba9070c3fc876
temasictfic <temasictfic@gmail.com>
2024-02-16 02:26:57 +0300

    Add reconnect and disconnect functionality to PongConsumer

--- indianpong/pong/consumers.py
+++ indianpong/pong/consumers.py
@@ -1,3 +1,4 @@
+import asyncio
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
@@ -20,14 +21,36 @@ class PongConsumer(AsyncWebsocketConsumer):
             await self.close()
         # Set the user's channel name
         await USER_CHANNEL_NAME.set(self.user.username, self.channel_name)
-        #self.user.channel_name = self.channel_name
-        #await self.user.asave()
         # Accept the connection
         await self.accept()
         # Add the user to the 'online' group
         await self.channel_layer.group_add("online", self.channel_name)
-        # Set the user's status to 'online'
-        await USER_STATUS.set(self.user.username, 'online')
+        # Check if the user is in a game
+        game_id = await USER_STATUS.get(self.user.username)
+        if game_id != 'online' and game_id != None:
+            # Get the game from the cache
+            game = await GAMES.get(game_id)
+            other_player = game.otherPlayer(self.user.username)
+            # Check if the game is paused and the other player is still in game
+            if game.status == Status.PAUSED and await USER_STATUS.get(other_player) == game_id:
+                await USER_STATUS.set(self.user.username, game_id)
+                # Add the user to the game group
+                await self.channel_layer.group_add(game.group_name, self.channel_name)
+                self.room_group_name = game.group_name
+                # Change the game status to 'started'
+                game.status = Status.STARTED
+                # Send the reconneted message to the other player
+                await self.channel_layer.group_send(
+                    game.group_name,
+                    {
+                        "type": "user.reconnected",
+                        "username": self.user.username,
+                    }
+                )
+        else:
+            # Set the user's status to 'online'
+            await USER_STATUS.set(self.user.username, 'online')
+
         # Send a message to the group with the user's username
         online_users = await USER_STATUS.get_keys_with_value('online')
         await self.channel_layer.group_send(
@@ -40,29 +63,48 @@ class PongConsumer(AsyncWebsocketConsumer):
         )
 
     async def disconnect(self, close_code):
-        #self.user.channel_name = None
-        game_id = await USER_STATUS.get(self.user.username)
-        if game_id != 'online' and game_id != None:
-            # Check if user's username is in any game and that game is not ended
-            game = await GAMES.get(game_id)
-            if game.status == Status.STARTED: # 2 means game is started
-                await self.record_for_disconnected(game_id, game)
-            # Delete game cache
-            await GAMES.delete(game_id)
-            # Remove both users from the game group
-            player1_channel_name = await USER_CHANNEL_NAME.get(game.player1.username)
-            player2_channel_name = await USER_CHANNEL_NAME.get(game.player2.username)
-            await self.channel_layer.group_discard(game.group_name, player1_channel_name)
-            await self.channel_layer.group_discard(game.group_name, player2_channel_name)
-        # Remove the user's channel name
+        # Remove the user from USER_CHANNEL_NAME and USER_STATUS
         await USER_CHANNEL_NAME.delete(self.user.username)
-        # make it offline
-        await USER_STATUS.delete(self.user.username) 
         # Remove the user from the 'online' group
         await self.channel_layer.group_discard("online", self.channel_name)
+        # Remove the user from the game group
         if self.room_group_name != None:
             # Leave room group
             await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+
+        # Close the websocket connection
+        await self.close(close_code)
+
+        game_id = await USER_STATUS.get(self.user.username)
+        if game_id != 'online' and game_id != None:
+            game = await GAMES.get(game_id)
+            #Send opponent a message that the user has disconnected
+            await self.channel_layer.group_send(
+                game.group_name,
+                {
+                    "type": "user.disconnected",
+                    "username": self.user.username,
+                }
+            )
+            if game.status == Status.STARTED: # 2 means game is started
+                # Pause the game
+                game.status = Status.PAUSED #game.pauseGame() #??? implement maybe this
+                # Wait 10 seconds for the other player to reconnect
+                await asyncio.sleep(10)
+                # Check if the player has reconnected if not, record the game as disconnected
+                if await USER_STATUS.get(self.user.username) != game_id:
+                    # Record the game as disconnected
+                    await self.record_for_disconnected(game_id, game)
+                    # Delete game cache
+                    await GAMES.delete(game_id)
+                    # Remove both users from the game group
+                    player1_channel_name = await USER_CHANNEL_NAME.get(game.player1.username)
+                    player2_channel_name = await USER_CHANNEL_NAME.get(game.player2.username)
+                    await self.channel_layer.group_discard(game.group_name, player1_channel_name)
+                    await self.channel_layer.group_discard(game.group_name, player2_channel_name)
+                    await USER_STATUS.delete(self.user.username)
+        else:
+            await USER_STATUS.delete(self.user.username)
         # Send a message to the group with the user's username
         await self.channel_layer.group_send(
             "online",
@@ -572,6 +614,24 @@ class PongConsumer(AsyncWebsocketConsumer):
         # Send a message to the client with the username
         await self.send(text_data=json.dumps({
             "type": "user.offline",
+            "username": username,
+        }))
+
+    async def user_reconnected(self, event):
+        # Handle a message that a user is reconnected
+        username = event["username"]
+        # Send a message to the client with the username
+        await self.send(text_data=json.dumps({
+            "type": "user.reconnected",
+            "username": username,
+        }))
+
+    async def user_disconnected(self, event):
+        # Handle a message that a user is disconnected
+        username = event["username"]
+        # Send a message to the client with the username
+        await self.send(text_data=json.dumps({
+            "type": "user.disconnected",
             "username": username,
         }))
 
