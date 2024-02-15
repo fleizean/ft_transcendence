346192e967a110070f9dc99db423bc997919a421
temasictfic <temasictfic@gmail.com>
2024-02-16 02:42:03 +0300

    Update routing.py and migrations/0001_initial.py

--- indianpong/indianpong/routing.py
+++ indianpong/indianpong/routing.py
@@ -2,13 +2,10 @@
 from django.urls import re_path
 from django.urls import path
 
-from pong import consumers, consumers2
-
-""" websocket_urlpatterns = [
-    re_path(r'ws/pong/$', consumers.PongConsumer.as_asgi()),
-    re_path(r'^ws/chat/(?P<room_name>[\w-]+)/$', consumers.PongConsumer.as_asgi()),
-] """
+from pong import consumers
 
 websocket_urlpatterns = [
-    path("ws/play/<room_code>/", consumers2.MultiplexerConsumer.as_asgi()),
+    re_path(r'ws/pong/$', consumers.PongConsumer.as_asgi()),
+    re_path(r'^ws/chat/(?P<room_name>[\w-]+)/$', consumers.PongConsumer.as_asgi()),
 ]
+
