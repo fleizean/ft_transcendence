fb7312628d922b2afb8f97fc9e47a11f03b0de24
Aktass <yunusemreaktas97@gmail.com>
2024-02-17 02:27:00 +0300

    Update .gitignore and Dockerfile, and modify search.html
    Co-authored-by: temasictfic <temasictfic@gmail.com>

--- indianpong/start.sh
+++
@@ -1,8 +0,0 @@
-python3 manage.py makemigrations
-python3 manage.py migrate
-python3 manage.py createsuperuser \
-        --noinput \
-        --username $DJANGO_SUPERUSER_USERNAME \
-		--display_name $DJANGO_SUPERUSER_DISPLAY_NAME
-python3 manage.py indianai
-exec "$@"
