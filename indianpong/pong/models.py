1f61c7ab1681b9889238de4172cb06ec5088124e
Enes <nsyagz@gmail.com>
2024-03-05 19:32:51 +0300

    storedata localization

--- indianpong/pong/models.py
+++ indianpong/pong/models.py
@@ -26,8 +26,14 @@ class Social(models.Model):
 class StoreItem(models.Model):
     category_name = models.CharField(max_length=100, default="")
     name = models.CharField(max_length=100)
+    name_hi = models.CharField(max_length=100, blank=True, null=True)
+    name_pt = models.CharField(max_length=100, blank=True, null=True)
+    name_tr = models.CharField(max_length=100, blank=True, null=True)
     image_url = models.TextField()
     description = models.TextField()
+    description_hi = models.TextField(blank=True, null=True)
+    description_pt = models.TextField(blank=True, null=True)
+    description_tr = models.TextField(blank=True, null=True)
     price = models.IntegerField()
     keypress = models.CharField(max_length=100, blank=True, null=True)
     show_status = models.BooleanField(default=False) # store'da görünebilir mi?
