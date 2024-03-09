2a473b22c424fe851936839d85cdb127befdab5a
Aktass <yunusemreaktas97@gmail.com>
2024-03-10 00:26:50 +0300

    allowed host added.

--- indianpong/indianpong/settings.py
+++ indianpong/indianpong/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = environ.get("SECRET_KEY", default="w^bxst+y6yv=d*5+7h)2s3)5vfz!b2ja
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = environ.get("DEBUG", default=True)
 
-ALLOWED_HOSTS = []#environ.get("ALLOWED_HOSTS", default="").split(" ")
+ALLOWED_HOSTS = ['indianpong.onrender.com']#environ.get("ALLOWED_HOSTS", default="").split(" ")
 
 
 # Application definition
