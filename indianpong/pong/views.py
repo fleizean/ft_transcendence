2d8de3f5e8d6f3e3cb3fd1212756792f8254c75b
temasictfic <temasictfic@gmail.com>
2024-03-17 18:41:41 +0300

    Add customizations and abilities to remote game view

--- indianpong/pong/views.py
+++ indianpong/pong/views.py
@@ -891,8 +891,24 @@ def local_tournament(request):
 def remote_game(request, game_type, game_id):
     lang = request.COOKIES.get('selectedLanguage', 'en')
     context = langs.get_langs(lang)
+    user_items = UserItem.objects.filter(user=request.user)
+    
+    # Just Customizations - PONG
+    ainametag = get_equipped_item_value(user_items, "My Beautiful AI", "IndianAI")
+    paddlecolor = get_equipped_item_value(user_items, "My Beautiful Paddle", "black")
+    playgroundcolor = get_equipped_item_value(user_items, "My Playground", "lightgrey")
+    
+    
+    # Just Abilities - PONG
+    giantman = get_equipped_item_value(user_items, "Giant-Man", "None")
+    likeacheater = get_equipped_item_value(user_items, "Like a Cheater", "None")
+    fastandfurious = get_equipped_item_value(user_items, "Fast and Furious", "None")
+    rageoffire = get_equipped_item_value(user_items, "Rage of Fire", "None")
+    frozenball = get_equipped_item_value(user_items, "Frozen Ball", "None")
+    givemethemusic = get_equipped_item_value(user_items, "DJ Give Me The Music", "None")
 
-    return render(request, "remote-game.html", {"context": context})
+    return render(request, "remote-game.html", {"ainametag": ainametag, "paddlecolor": paddlecolor, "playgroundcolor": playgroundcolor, "giantman": giantman, "likeacheater": likeacheater, "fastandfurious": fastandfurious, "rageoffire": rageoffire, "frozenball": frozenball, "givemethemusic": givemethemusic, "context": context})
+
 
 @never_cache
 @login_required()
