888e5144de12565d84c4f8a63de243a895a60a9b
temasictfic <temasictfic@gmail.com>
2024-03-30 06:34:47 +0300

    [Module script system] Update loading bar style and add script type to login and signup pages, file size 2mb
    Co-authored-by: Aktaş <yeaktas@users.noreply.github.com>

---
+++ indianpong/static/js/login.js
@@ -0,0 +1,59 @@
+export function initializeLogin() {
+  const passwordInput = document.getElementById('id_password');
+  const toggleButton = document.getElementById('togglePassword');
+
+  toggleButton.addEventListener('mousedown', function() {
+    passwordInput.type = 'text';
+    toggleButton.classList.remove('bi-eye-slash-fill');
+    toggleButton.classList.add('bi-eye-fill');
+  });
+
+  toggleButton.addEventListener('mouseup', function() {
+    passwordInput.type = 'password';
+    toggleButton.classList.remove('bi-eye-fill');
+    toggleButton.classList.add('bi-eye-slash-fill');
+  });
+  passwordInput.addEventListener('input', function() {
+    toggleButton.style.display = this.value.trim() !== '' ? 'block' : 'none';
+  });
+}
+
+function showToast(content, status, iconClass) {
+  const liveToast = document.getElementById('liveToast');
+  var toastContent = document.querySelector('#liveToast .fw-semibold');
+  var toastIcon = document.querySelector('.toast-body .i-class i');
+
+  toastIcon.className = iconClass;
+  liveToast.classList.remove('text-bg-danger'); 
+  liveToast.className = 'toast'; 
+  liveToast.classList.add(status);
+
+  toastContent.textContent = content;
+
+  const toast = new bootstrap.Toast(liveToast);
+  toast.show();
+  }
+
+export function makeLogin() {
+  var form = document.getElementById('loginForm');
+  var formData = new FormData(form);
+  var csrftoken = document.cookie.split('; ').find(row => row.startsWith('csrftoken')).split('=')[1];
+  fetch('/login', {
+      method: 'POST',
+      headers: {
+        'X-CSRFToken': csrftoken
+      },
+      body: formData
+    })
+    .then(response => response.text())
+    .then(data => {
+      if (data == "dashboard")
+        swapApp('/dashboard');
+      else {
+        showToast(`${data}`, `text-bg-danger`, `bi bi-bug-fill`);
+      }
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
--- indianpong/static/js/navigation.js
+++ indianpong/static/js/navigation.js
@@ -1,113 +1,55 @@
+import { initializeLogin, makeLogin } from './login.js';
+import { initializeSignup, makeRegister } from './signup.js';
+
 function updateApp(path) {
-    fetch(path)
-        .then(response => response.text())
-        .then(html => {
-            document.body.innerHTML = html;
-            if (path == '/signup')
-                initializePage();
-        })
-        .catch(error => console.error(error));
+  showLoadingScreen();
+  fetch(path)
+    .then(response => response.text())
+    .then(html => {
+      document.body.innerHTML = html;
+      pageHandler(path);
+      hideLoadingScreen();
+    })
+    .catch(error => console.error(error));
 }
 
 function swapApp(path) {
-    currentPath = window.location.pathname;
-    window.history.pushState({}, '', path);
-       
-    updateApp(path);
+  //currentPath = window.location.pathname;
+  window.history.pushState({}, '', path);
+  updateApp(path);
 }
 
-function initializePage() {
-    const fileInput = document.getElementById('id_avatar');
-    if (fileInput) {
-        fileInput.addEventListener('change', function(event) {
-            const selectedLanguage = document.cookie.split('; ').find(row => row.startsWith('selectedLanguage')).split('=')[1];
-            var file = this.files[0];
-            if (file.size > 2.6 * 1024 * 1024) {
-                var messages = {
-                    'en': "Photo must be under 2.6MB!",
-                    'tr': "Fotoğraf 2.6MB'dan küçük olmalıdır!",
-                    'hi': "फोटो 2.6MB से कम होना चाहिए!",
-                    'pt': "A foto deve ter menos de 2,6 MB!"
-                };
-                var message = messages[selectedLanguage] || "Photo must be under 2.6MB!";
-                showToast(message, "text-bg-danger", "bi bi-image");
-                this.value = '';
-            }
-            else {
-                var messages = {
-                    'en': "Photo added!",
-                    'tr': "Fotoğraf eklendi!",
-                    'hi': "फोटो जोड़ दी गई!",
-                    'pt': "Foto adicionada!"
-                };
-                var message = messages[selectedLanguage] || "Photo added!";
-                showToast(message, "text-bg-success", "bi bi-image");
-            }
-        });
-    }
-
-    
-}
-
+window.swapApp = swapApp;
+window.initializeLogin = initializeLogin;
+window.initializeSignup = initializeSignup;
+window.makeLogin = makeLogin;
+window.makeRegister = makeRegister;
 
 window.onpopstate = function(event) {
-    updateApp(window.location.pathname);
+  updateApp(window.location.pathname);
 };
 
 function setLanguage(language) {
-    document.cookie = "selectedLanguage=" + language;
-    swapApp(window.location.pathname);
+  document.cookie = "selectedLanguage=" + language;
+  swapApp(window.location.pathname);
 }
 
-function makeLogin() {
-    var form = document.getElementById('loginForm');
-    var formData = new FormData(form);
 
-    var csrftoken = document.cookie.split('; ').find(row => row.startsWith('csrftoken')).split('=')[1];
 
-    fetch('/login', {
-      method: 'POST',
-      headers: {
-        'X-CSRFToken': csrftoken
-      },
-      body: formData
-    })
-    .then(response => response.text())
-    .then(data => {
-      if (data == "dashboard")
-        swapApp('/dashboard');
-      else {
-        showToast(`${data}`, `text-bg-danger`, `bi bi-bug-fill`);
-      }
-    })
-    .catch(error => {
-      console.error(error);
-    });
-  }
-  
-function makeRegister() {
-    var form = document.getElementById('registerForm');
-    var formData = new FormData(form);
+function pageHandler(path) {
+    if (path == '/login') {
+      initializeLogin();
+    }
+    else if (path == '/signup') {
+      initializeSignup();
+    }
+}
 
-    var csrftoken = document.cookie.split('; ').find(row => row.startsWith('csrftoken')).split('=')[1];
+function showLoadingScreen() {
+  document.getElementById('loading-bar').style.width = '100%';;
+}
 
-    fetch('/signup', {
-      method: 'POST',
-      headers: {
-        'X-CSRFToken': csrftoken
-      },
-      body: formData
-    })
-    .then(response => response.text())
-    .then(data => {
-      if (data == "login") {
-        swapApp('/login');
-      }
-      else {
-        document.body.innerHTML = data;
-      }
-      })
-    .catch(error => {
-      console.error(error);
-    });
-  }
+// Yükleme ekranını gizle
+function hideLoadingScreen() {
+  document.getElementById('loading-bar').style.width = '0';;
+}
